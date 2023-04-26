eacc1cbdcaa47d51a607a0fa52893dbd67276f68
vtsuellen <vtsuellen@hotmail.com>
2023-04-26 17:25:44 -0300

    erros ok

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Valide o formulário
-const form = document.querySelector(".trybewarts-login");
+const form = document.querySelector('.trybewarts-login');
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#senha").value;
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#senha').value;
 
   if (email === 'tryber@teste.com' && password === '123456') {
     alert('Olá, Tryber!');
