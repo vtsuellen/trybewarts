0336e6137114206149415dae9d179e2c5d4587ac
vtsuellen <vtsuellen@hotmail.com>
2023-04-28 22:19:41 -0300

    ultimo requisito

--- script.js
+++ script.js
@@ -43,3 +43,71 @@ agreementCheckbox.addEventListener('change', () => {
 });
 
 // Substitua o formulário pelas informações da pessoa estudante
+
+const btn = document.querySelector('#submit-btn');
+const formPrincipal = document.querySelector('#evaluation-form');
+const formSecundario = document.querySelector('#form-data');
+const returnName = document.querySelector('#input-name');
+const returnLastname = document.querySelector('#input-lastname');
+const returnEmail = document.querySelector('#input-email');
+const returnHouse = document.querySelector('#house');
+const returnFamily = document.getElementsByName('family');
+const returnContent = document.getElementsByClassName('subject');
+const returnEvaluation = document.getElementsByName('rate');
+const returnObs = document.querySelector('#textarea');
+const submitNome = document.querySelector('#returnNome');
+const submitEmail = document.querySelector('#returnEmail');
+const submitCasa = document.querySelector('#returnCasa');
+const submitFamilia = document.querySelector('#returnFamilia');
+const submitMateria = document.querySelector('#returnMaterias');
+const submitAvaliacao = document.querySelector('#returnAvaliacao');
+const submitObs = document.querySelector('#returnObs');
+
+const enviaFamilia = () => {
+  let familia = '';
+  for (let i = 0; i < returnFamily.length; i += 1) {
+    if (returnFamily[i].checked === true) {
+      familia = returnFamily[i].value;
+    }
+  }
+  submitFamilia.innerHTML += familia;
+};
+
+const enviaMateria = () => {
+  let materia = '';
+  for (let index = 0; index < returnContent.length; index += 1) {
+    if (returnContent[index].checked === true) {
+      materia += `${returnContent[index].value}, `;
+    }
+  }
+  submitMateria.innerHTML += materia;
+};
+
+const enviaAvaliacao = () => {
+  let avaliacao = '';
+  for (let i = 0; i < returnEvaluation.length; i += 1) {
+    if (returnEvaluation[i].checked === true) {
+      avaliacao = returnEvaluation[i].value;
+    }
+  }
+  submitAvaliacao.innerHTML += avaliacao;
+};
+
+const enviarForm = (event) => {
+  // Não atualiza a pagina ao click
+  event.preventDefault();
+  // Adiciona display none no formulario da pagina principal
+  formPrincipal.style.display = 'none';
+  formSecundario.style.display = 'flex';
+
+  submitNome.innerHTML += `${returnName.value} ${returnLastname.value}`;
+  submitEmail.innerHTML += returnEmail.value;
+  submitCasa.innerHTML += returnHouse.value;
+  submitObs.innerHTML += returnObs.value;
+
+  enviaFamilia();
+  enviaAvaliacao();
+  enviaMateria();
+};
+
+formPrincipal.addEventListener('submit', enviarForm);
